b9b10d3515449aa92421956ecf92c8bc40542924
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-06-27 12:07:16 +0300

    Proper signin logic

--- src/repositories/auth/repository.py
+++ src/repositories/auth/repository.py
@@ -2,10 +2,11 @@ __all__ = ["SqlAuthRepository", "auth_repository"]
 
 from typing import Self
 
-from sqlalchemy import select
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.storage.sql.models.user import User
+from src.schemas.signin import CreateProfile, Signin
+from src.storage.sql.models import User, Profile
 from src.storage.sql.storage import AbstractSQLAlchemyStorage
 
 
@@ -18,13 +19,30 @@ class SqlAuthRepository:
 
     def _create_session(self) -> AsyncSession:
         return self.storage.create_session()
-    
-    async def signin(self, t_id: int, username: str):
+
+    async def signin(self, signin: Signin):
         async with self._create_session() as session:
-            query = select(User).where(User.Tg_id == t_id)
-            obj = await session.scalar(query)
-            if not obj:
-                session.add(User(Tg_id=t_id, User_name=username))
+            query = select(User).where(User.tg_id == signin.tg_id)
+            user_obj = await session.scalar(query)
+
+            if not user_obj:
+                profile = Profile(
+                    **CreateProfile(
+                        first_name=signin.first_name,
+                        last_name=signin.last_name,
+                        username=signin.username,
+                        photo_url=signin.photo_url,
+                    ).model_dump()
+                )
+                session.add(profile)
+                await session.commit()
+                
+                session.add(
+                    User(
+                        tg_id=signin.tg_id,
+                        profile_id=profile.id,
+                    )
+                )
                 await session.commit()
 
 
