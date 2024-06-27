7bfae0d38f9df3a83ce00d5618b3baf7a8110d2e
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-06-27 12:33:19 +0300

    Made routes more robust

--- src/api/profiles/profileRoutes.py
+++ src/api/profiles/profileRoutes.py
@@ -1,23 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 
-from src.schemas.profile import UserProfile
+from src.schemas.signin import CreateProfile
 from src.repositories.profiles.profileRepository import profile_repository
 
 profileRouter = APIRouter(prefix="/profile")
 
+
 @profileRouter.post("/create")
-async def createProfile(profile: UserProfile):
+async def createProfile(profile: CreateProfile, status_code=201):
     await profile_repository.create(profile)
 
-@profileRouter.get("/get/{id}")
-async def getProfile(id: int):
-    return await profile_repository.get(id)
 
-@profileRouter.put("/update/{id}")
-async def updateProfile(id: int, profile: UserProfile):
-    await profile_repository.update(id, profile)
+@profileRouter.get("/get/{id}")
+async def getProfile(id: int, response: Response):
+    result = await profile_repository.get(id)
+    if not result:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return result if result else {}
+
+
+@profileRouter.put("/update/{id")
+async def updateProfile(id: int, profile: CreateProfile, response: Response):
+    result = await profile_repository.update(id, profile)
+    if not result:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return result if result else {}
+
 
 @profileRouter.delete("/delete/{id}")
 async def deleteProfile(id: int):
     await profile_repository.delete(id)
-
+    return {}
--- src/repositories/profiles/profileRepository.py
+++ src/repositories/profiles/profileRepository.py
@@ -1,14 +1,13 @@
 __all__ = ["SqlProfileRepository", "profile_repository"]
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.schemas.signin import CreateProfile
 from src.storage.sql.models import Profile
 from src.storage.sql.storage import AbstractSQLAlchemyStorage
-from typing import Self, Type
+from typing import Self
 
-from sqlalchemy import select
 from src.storage.sql.models.profile import Profile
-from src.schemas.profile import UserProfile
 
-from sqlalchemy.ext.asyncio import AsyncSession
 
 class SqlProfileRepository:
     storage: AbstractSQLAlchemyStorage
@@ -20,39 +19,25 @@ class SqlProfileRepository:
     def _create_session(self) -> AsyncSession:
         return self.storage.create_session()
 
-    async def create(self, profile: UserProfile) -> None:
+    async def create(self, profile: CreateProfile) -> Profile:
         async with self._create_session() as session:
-            profile = Profile(
-                name=profile.name,
-                age=profile.age,
-                photo=profile.photo,
-                date_birth=profile.date_birth,
-                sex=profile.sex,
-                religion=profile.religion,
-                about=profile.about
-            )
+            profile = Profile(**profile.model_dump())
             session.add(profile)
             await session.commit()
+            return profile
 
-    async def get(self, id: int) -> Profile:
+    async def get(self, id: int) -> Profile | None:
         async with self._create_session() as session:
             profile = await session.get(Profile, id)
             return profile if profile else None
 
-
-    async def update(self, id: int, profile: UserProfile) -> None:
+    async def update(self, id: int, profile: CreateProfile) -> Profile | None:
         async with self._create_session() as session:
             db_profile = await session.get(Profile, id)
             if db_profile:
-                db_profile.name = profile.name
-                db_profile.age = profile.age
-                db_profile.photo = profile.photo
-                db_profile.date_birth = profile.date_birth
-                db_profile.sex = profile.sex
-                db_profile.religion = profile.religion
-                db_profile.about = profile.about
+                db_profile = Profile(**profile.model_dump())
                 await session.commit()
-
+                return db_profile
 
     async def delete(self, id: int) -> None:
         async with self._create_session() as session:
@@ -62,6 +47,4 @@ class SqlProfileRepository:
                 await session.commit()
 
 
-
 profile_repository: SqlProfileRepository = SqlProfileRepository()
-
