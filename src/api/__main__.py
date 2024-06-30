7fb27107cecea7728276de34a0b329ec7e527959
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-06-30 10:06:49 +0300

    SSL support

--- src/api/__main__.py
+++ src/api/__main__.py
@@ -9,13 +9,23 @@ os.chdir(Path(__file__).parents[2])
 # Get arguments from command
 args = sys.argv[1:]
 
+ssl_args = (
+    [
+        "--ssl-ca-certs=./certs/certificate_ca.crt",
+        "--ssl-keyfile=./certs/certificate.key",
+        "--ssl-certfile=./certs/certificate.crt",
+    ]
+    if os.getenv("PROD")
+    else []
+)
+
 uvicorn.main.main(
     [
         "src.api.app:app",
         "--use-colors",
         "--proxy-headers",
         "--forwarded-allow-ips=*",
-        # "--log-config=logging.yaml",
+        *ssl_args,
         *args,
     ]
 )
