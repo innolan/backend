6b49c07ca87f77a0fd5596f8fc1fa58faeaae0ca
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-03 13:05:16 +0300

    Proposed logical changes to DB structure

--- src/storage/sql/models/profile.py
+++ src/storage/sql/models/profile.py
@@ -11,10 +11,7 @@ from src.storage.sql.models import Base
 class Profile(Base, IdMixin):
     __tablename__ = "profiles"
 
-    first_name: Mapped[str] = mapped_column(String(100))
-    last_name: Mapped[str] = mapped_column(String(100))
-    username: Mapped[str] = mapped_column(String(50))
-    photo_url: Mapped[str] = mapped_column(String(200))
+    # Attributes
     date_of_birth: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         nullable=True,
@@ -23,5 +20,6 @@ class Profile(Base, IdMixin):
     religion: Mapped[str] = mapped_column(String(40), nullable=True)
     about: Mapped[str] = mapped_column(String(2000), nullable=True)
 
+    # Relations
     profile_user = relationship("User", back_populates="user_profile")
     profile_metric = relationship("Metric", back_populates="metric_profile")
