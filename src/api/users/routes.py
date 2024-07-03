0d4054c2c99c560354b92bd232a488f2d62c8052
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-03 22:52:09 +0300

    Update logic

--- src/api/users/routes.py
+++ src/api/users/routes.py
@@ -3,7 +3,7 @@ __all__ = ["router"]
 from fastapi import APIRouter, Response, status
 
 from src.repositories.user.repository import user_repository
-from src.schemas.userinfo import UserInfo
+from src.schemas.userinfo import UpdateUserInfo, UserInfo
 
 router = APIRouter(prefix="/users")
 
@@ -11,6 +11,13 @@ router = APIRouter(prefix="/users")
 @router.get("/get/{id}")
 async def getUserInfo(id: int, response: Response) -> UserInfo | None:
     result = await user_repository.get(id)
+    if not result:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return result if result else {}
+
+@router.get("/update/{id}")
+async def getUserInfo(id: int, update: UpdateUserInfo) -> UserInfo | None:
+    result = await user_repository.update(id, update)
     if not result:
         response.status_code = status.HTTP_404_NOT_FOUND
     return result if result else {}
--- src/repositories/user/repository.py
+++ src/repositories/user/repository.py
@@ -5,7 +5,7 @@ from typing import Self
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.schemas.userinfo import ProcessedMetric, UserInfo
+from src.schemas.userinfo import ProcessedMetric, UpdateUserInfo, UserInfo
 from src.storage.sql.models import User, Profile, Metric
 from src.storage.sql.storage import AbstractSQLAlchemyStorage
 
@@ -48,6 +48,53 @@ class SqlUserRepository:
             # )
 
             # Merge everything into a UserInfo object
+            return UserInfo(
+                first_name=user.first_name,
+                last_name=user.last_name,
+                username=user.username,
+                photo_url=user.photo_url,
+                about=profile.about,
+                date_of_birth=profile.date_of_birth,
+                sex=profile.sex,
+                religion=profile.religion,
+                hobby=profile.hobby,
+                soc_media=profile.soc_media,
+                # metrics=metrics,
+            )
+
+    async def update(self, id: int, update: UpdateUserInfo) -> UserInfo | None:
+        async with self._create_session() as session:
+            # Get user
+            user = await session.get(User, id)
+            if not user:
+                return
+                # raise NoUserException # TODO
+
+            # Get profile
+            profile = await session.get(Profile, user.profile_id)
+            # TODO
+            if not profile:
+                return
+                # raise NoProfileException # TODO
+
+            # Update
+            user.first_name = update.first_name | user.first_name
+            user.last_name = update.last_name | user.last_name
+            user.username = update.username | user.username
+            user.photo_url = update.photo_url | user.photo_url
+
+            profile.date_of_birth = update.date_of_birth | profile.date_of_birth
+            profile.sex = update.sex | profile.sex
+            profile.religion = update.religion | profile.religion
+            profile.hobby = update.hobby | profile.hobby
+            profile.soc_media = update.soc_media | profile.soc_media
+            # metrics = update.metrics | profile.metrics
+
+            session.add(user)
+            session.add(profile)
+
+            await session.commit()
+
             return UserInfo(
                 first_name=user.first_name,
                 last_name=user.last_name,
