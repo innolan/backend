71c70b08486d06c8fafc06296f47e325ec1eed97
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-03 14:52:10 +0300

    Cleaned up model code

--- src/storage/sql/models/metric.py
+++ src/storage/sql/models/metric.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -5,15 +6,14 @@ from src.storage.sql.__mixin__ import IdMixin
 from src.storage.sql.models.base import Base
 
 
+@dataclass
 class Metric(Base, IdMixin):
     __tablename__ = "metrics"
 
-    profile_id: Mapped[int] = mapped_column(
-        ForeignKey("profiles.id"),
-        nullable=True,
-    )
-
+    # Attributes
+    profile_id: Mapped[int] = mapped_column(ForeignKey("profiles.id"), nullable=True)
     name: Mapped[str] = mapped_column(String(40))
     value: Mapped[float] = mapped_column()
 
-    metric_profile = relationship("Profile", back_populates="profile_metric")
+    # Relations
+    _metric_profile = relationship("Profile", back_populates="_profile_metric")
--- src/storage/sql/models/profile.py
+++ src/storage/sql/models/profile.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
 from datetime import datetime
+from typing import List, Optional
 
-from sqlalchemy import DateTime, String
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -8,14 +10,12 @@ from src.storage.sql.__mixin__ import IdMixin
 from src.storage.sql.models import Base
 
 
+@dataclass
 class Profile(Base, IdMixin):
     __tablename__ = "profiles"
 
     # Attributes
-    date_of_birth: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        nullable=True,
-    )
+    date_of_birth: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
     sex: Mapped[int] = mapped_column(nullable=True)
     religion: Mapped[str] = mapped_column(String(40), nullable=True)
     about: Mapped[str] = mapped_column(String(2000), nullable=True)
@@ -23,5 +23,5 @@ class Profile(Base, IdMixin):
     soc_media: Mapped[list[str]] = mapped_column(ARRAY(String), nullable=True)
 
     # Relations
-    profile_user = relationship("User", back_populates="user_profile")
-    profile_metric = relationship("Metric", back_populates="metric_profile")
+    _profile_user = relationship("User", back_populates="_user_profile")
+    _profile_metric = relationship("Metric", back_populates="_metric_profile")
