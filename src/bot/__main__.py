f454770218b88a46cfc8710ee17185d17e3f80ef
simelles <slaval4745@gmail.com>
2024-07-04 12:04:31 +0300

    add menu button with webapp

--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -3,9 +3,15 @@ from logging import basicConfig, INFO
 from os import getenv
 
 from aiogram import Dispatcher, Bot
+from aiogram.types import MenuButtonWebApp, WebAppInfo
 from dotenv import load_dotenv
 
 from src.bot.app.handlers import router
+
+
+async def menu(bot: Bot):
+    await bot.set_chat_menu_button(
+        menu_button=MenuButtonWebApp(text='Web App', web_app=WebAppInfo(url='https://innolan.ru')))
 
 
 async def on_startup() -> None:
@@ -13,6 +19,7 @@ async def on_startup() -> None:
     basicConfig(level=INFO)
     bot = Bot(token=getenv('BOT_TOKEN'))
     dp = Dispatcher()
+    dp.startup.register(menu)
     dp.include_router(router)
     await dp.start_polling(bot)
 
--- src/bot/app/keyboard.py
+++ src/bot/app/keyboard.py
@@ -11,7 +11,7 @@ start = InlineKeyboardMarkup(
                 text="Registration",
                 login_url=LoginUrl(
                     url="https://innolan.ru/api/auth/signin",
-                    bot_username="innolan_staging_bot",
+                    bot_username="innolan_bot",
                     request_write_access=True,
                 ),
             )
