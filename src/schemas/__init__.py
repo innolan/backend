c01823fcb9cb5764484fd0ba5917876c178278ad
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-09 13:25:29 +0300

    Global schema import

---
+++ src/schemas/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "MetricDTO",
+    "ProfileDTO",
+    "UserDTO",
+    "WebAppInitData",
+    "WebAppUser",
+]
+
+from src.schemas.metric import MetricDTO
+from src.schemas.profile import ProfileDTO
+from src.schemas.user import UserDTO
+from src.schemas.webapp import WebAppInitData, WebAppUser
