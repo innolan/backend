d4f3a49595c8de7dfee7769ea815dfd63a54e1d7
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-09 21:20:32 +0300

    Added user info repo

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,11 +1,14 @@
 from src.repositories.auth.repository import auth_repository
 from src.repositories.metric.repository import metric_repository
 from src.repositories.profile.repository import profile_repository
+from src.repositories.userinfo.repository import userinfo_repository
 from src.repositories.user.repository import user_repository
+
 
 __all__ = [
     "auth_repository",
     "metric_repository",
     "profile_repository",
     "user_repository",
+    "userinfo_repository",
 ]
