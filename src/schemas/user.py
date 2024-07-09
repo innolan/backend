c3c4d4f50a31cf8dbbb82e23b700360df0e11204
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-09 12:58:53 +0300

    Separate UserDTO

---
+++ src/schemas/user.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from pydantic import BaseModel
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class UserDTO(BaseModel):
+    id: int
+    first_name: str
+    last_name: Optional[str] = None
+    username: Optional[str] = None
+    auth_date: int
+    photo_url: Optional[str] = None
+    
+    profile_id: Optional[int] = None
+
+    def verify_password(self, password: str):
+        return pwd_context.verify(password, self.auth_date)
+
+    @classmethod
+    def get_password_hash(cls, password):
+        return pwd_context.hash(password)
+    
+    class Config:
+            from_attributes = True
