6a4d9250d6fc3cd88a2b31a53ac273b44905296b
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-09 21:19:34 +0300

    Token schema

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -4,9 +4,13 @@ __all__ = [
     "UserDTO",
     "WebAppInitData",
     "WebAppUser",
+    "Signin",
+    "Token",
 ]
 
 from src.schemas.metric import MetricDTO
 from src.schemas.profile import ProfileDTO
 from src.schemas.user import UserDTO
 from src.schemas.webapp import WebAppInitData, WebAppUser
+from schemas.auth import Token
+
