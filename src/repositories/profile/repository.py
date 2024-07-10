1fc30c841befd13ac5ea64ecca738506ff6d1dce
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-10 19:10:20 +0300

    fix: Simplified Profile repo

--- src/repositories/profile/repository.py
+++ src/repositories/profile/repository.py
@@ -1,55 +1,23 @@
 __all__ = ["SqlProfileRepository", "profile_repository"]
 
 
-from asyncio import gather
-from sqlalchemy import delete, insert, select
+from deprecated import deprecated
+from sqlalchemy import delete, update
 
 from src import schemas
 from src.repositories.baserepo import SqlBaseRepository
 from src.storage.sql.models import Profile, Metric
-from src.exceptions import EntityExistsException, NoProfileException
+from src.exceptions import NotImplementedException
 from src.utils.compare_lists import compare_lists
 import src.repositories as reps
 
 
 class SqlProfileRepository(SqlBaseRepository):
-    async def add(self, create: schemas.ProfileDTO):
-        async with self._create_session() as session:
-            raw_profile = Profile(**create.model_dump(exclude={"metrics"}))
-            session.add(raw_profile)
-            await session.commit()
+    @deprecated("Profile creation is delegated to 'userinfo_repository.initialize()'")
+    async def add(self, add: schemas.ProfileDTO):
+        raise NotImplementedException()
 
-        
-            metrics: list[schemas.MetricDTO] = None
-            if create.metrics is not None:
-                # Write all metrics
-                metrics = await gather(
-                    *[
-                        reps.metric_repository.create(
-                            schemas.MetricDTO(
-                                name=m.name,
-                                value=m.value,
-                                profile_id=raw_profile.id,
-                            )
-                        )
-                        for m in create.metrics
-                    ]
-                )
-                # Strip metrics of profile_id
-                metrics = list(
-                    map(
-                        lambda m: schemas.MetricDTO(**m.model_dump(exclude={"profile_id"})),
-                        metrics,
-                    )
-                )
-                
-
-            await session.commit()
-            profile_dto = schemas.ProfileDTO.model_validate(raw_profile)
-            
-            profile_dto.metrics = metrics
-            return profile_dto
-
+    # TODO: Rewrite to use less sessions
     async def get(self, id: int):
         async with self._create_session() as session:
             raw_profile = await session.get(Profile, id)
@@ -67,33 +35,46 @@ class SqlProfileRepository(SqlBaseRepository):
             profile = await session.get(Profile, profile_id)
             return True if profile else False
 
-    async def update(self, update: schemas.ProfileDTO):
+    async def update(self, new_profile: schemas.ProfileDTO):
         async with self._create_session() as session:
-            profile = await self.get(update.id)
+            profile = await self.get(new_profile.id)
 
-            # Compare metrics
-            removed, added, _ = compare_lists(profile.metrics, update.metrics)
+            # 1. Update regular fields
+            profile.about = new_profile.about or profile.about
+            profile.date_of_birth = new_profile.date_of_birth or profile.date_of_birth
+            profile.sex = new_profile.sex or profile.sex
+            profile.religion = new_profile.religion or profile.religion
+            profile.hobby = new_profile.hobby or profile.hobby
+            profile.soc_media = new_profile.soc_media or profile.soc_media
 
-            # Delete irrelevant ones
-            await gather([reps.metric_repository.delete(m.id) for m in removed])
-
-            # Add relevant ones
-            await gather(
-                [
-                    reps.metric_repository.create(
-                        schemas.MetricDTO(
-                            name=m.name,
-                            value=m.value,
-                            profile_id=profile.id,
-                        )
-                    )
-                    for m in added
-                ]
+            session.execute(
+                update(Profile)
+                .where(Profile.id == profile.id)
+                .values(profile.model_dump(exclude={"id", "metric"}))
             )
+
+            # 2. Update metrics
+            if new_profile.metrics is not None:
+                # Compare metrics
+                removed, added, _ = compare_lists(profile.metrics, new_profile.metrics)
+
+                # Delete irrelevant ones
+                session.execute(
+                    delete(Metric).where(Metric.id.in_(map(lambda m: m.id, removed)))
+                )
+
+                # Add relevant ones
+                for m in added:
+                    metric = Metric(
+                        name=m.name,
+                        value=m.value,
+                        profile_id=profile.id,
+                    )
+                    session.add(metric)
 
             await session.commit()
 
-            return await self.get(update.id)
+            return await self.get(new_profile.id)
 
     async def delete(self, id: str):
         async with self._create_session() as session:
