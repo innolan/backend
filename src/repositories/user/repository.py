3afd71c9e93f6f042c6df78d73b1207b2a36e7c3
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-11 13:21:03 +0300

    Added user update method

--- src/repositories/user/repository.py
+++ src/repositories/user/repository.py
@@ -1,12 +1,13 @@
 __all__ = ["SqlUserRepository", "user_repository"]
 
-from sqlalchemy import delete
+from sqlalchemy import delete, update
 from deprecated import deprecated
 
 from src import schemas
 from src.storage.sql.models import User
 from src.exceptions import NotImplementedException
 from src.repositories.baserepo import SqlBaseRepository
+
 
 class SqlUserRepository(SqlBaseRepository):
     async def get(self, id: int, remove_hash=True):
@@ -23,9 +24,23 @@ class SqlUserRepository(SqlBaseRepository):
     async def add(self, user: schemas.UserDTO):
         raise NotImplementedException()
 
-    async def update(self, id: int, update: schemas.UserDTO):
-        # TODO
-        raise NotImplementedException()
+    async def update(self, id: int, new_user: schemas.UserDTO):
+        user = await self.get(id)
+
+        user.id = new_user.id or user.id
+        user.first_name = new_user.first_name or user.first_name
+        user.last_name = new_user.last_name or user.last_name
+        user.username = new_user.username or user.username
+        user.photo_url = new_user.photo_url or user.photo_url
+
+        async with self._create_session() as session:
+            await session.execute(
+                update(User)
+                .where(User.id == id)
+                .values(**user.model_dump(exclude={"id"}))
+            )
+            await session.commit()
+            return new_user
 
     async def delete(self, id: int):
         async with self._create_session() as session:
