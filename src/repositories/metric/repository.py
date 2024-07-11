5749d0abc67c62d03d6a3c83d5cfdb22959575aa
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-11 14:09:15 +0300

    Reactivated old method

--- src/repositories/metric/repository.py
+++ src/repositories/metric/repository.py
@@ -6,7 +6,7 @@ from src.repositories.baserepo import SqlBaseRepository
 
 from src import schemas
 from src import repositories as reps
-from src.exceptions import NoMetricException, NotImplementedException
+from src.exceptions import EntityNotFoundException, NoMetricException, NotImplementedException
 from src.storage.sql.models import Metric
 
 
@@ -42,22 +42,22 @@ class SqlMetricRepository(SqlBaseRepository):
 
     async def get_by_profile_id(self, profile_id: int):
         # TODO: Reconsider method
-        raise NotImplementedException()
-        # async with self._create_session() as session:
-        #     if not (await reps.profile_repository.is_profile_exist(profile_id)):
-        #         raise NoProfileException()
+        # raise NotImplementedException()
+        async with self._create_session() as session:
+            if not (await reps.profile_repository.is_profile_exist(profile_id)):
+                raise EntityNotFoundException('profile')
 
-        #     query = select(Metric).where(Metric.profile_id == profile_id)
-        #     scalars = (await session.execute(query)).scalars().all()
-        #     metrics = [schemas.MetricDTO.model_validate(s) for s in scalars]
-        #     # Strip metrics of profile_id
-        #     metrics = list(
-        #         map(
-        #             lambda m: schemas.MetricDTO(**m.model_dump(exclude={"profile_id"})),
-        #             metrics,
-        #         )
-        #     )
-        #     return metrics
+            query = select(Metric).where(Metric.profile_id == profile_id)
+            scalars = (await session.execute(query)).scalars().all()
+            metrics = [schemas.MetricDTO.model_validate(s) for s in scalars]
+            # Strip metrics of profile_id
+            metrics = list(
+                map(
+                    lambda m: schemas.MetricDTO(**m.model_dump(exclude={"profile_id"})),
+                    metrics,
+                )
+            )
+            return metrics
 
     async def update(self, profile_id: int, new_metric: schemas.MetricDTO):
         async with self._create_session() as session:
