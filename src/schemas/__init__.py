e34b287d9b88a3d49bad6c19bae0c96cf491923c
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-12 20:30:38 +0300

    fix: Reverted deletion

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -18,4 +18,5 @@ from src.schemas.metric import MetricDTO, MetricDTOAdd, MetricDTOUpd
 from src.schemas.profile import ProfileDTO, ProfileDTOUpd, ProfileDTORet
 from src.schemas.user import UserDTO
 from src.schemas.webapp import WebAppInitData, WebAppUser
+from src.schemas.auth import Token
 from src.schemas.userinfo import UserInfoDTO, UserInfoDTOUpd
