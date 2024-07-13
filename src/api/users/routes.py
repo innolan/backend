968b920f4667b257241cf13402c826a6e28f2959
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-13 10:38:40 +0300

    fix: Fixed docs

--- src/api/users/routes.py
+++ src/api/users/routes.py
@@ -1,14 +1,11 @@
 __all__ = ["router"]
 
-from typing import Annotated
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
-from src.middleware.auth_guard import get_id
-from src.repositories.user.repository import user_repository
-from src.exceptions import Message
-from src import repositories as reps
 from src import schemas
+from src import repositories as reps
 from src.utils import messages
+from src.middleware.auth_guard import get_id
 
 router = APIRouter(prefix="/user", tags=["Users"])
 
@@ -33,22 +30,38 @@ async def getUserInfo(id: str = Depends(get_id)):
     },
     response_model_exclude_none=True,
 )
-async def updateUserInfo(
-    userinfo: schemas.UserInfoDTOUpd,
-    id: str = Depends(get_id),
-):
+async def updateUserInfo(userinfo: schemas.UserInfoDTOUpd, id: str = Depends(get_id)):
     return await reps.userinfo_repository.update(id, userinfo)
 
-@router.put("/me/likes/{target_id}", response_model=schemas.UserInfoDTO)
+
+@router.put(
+    "/me/likes/{target_id}",
+    responses={
+        200: {"model": list[int]},
+        404: {"model": messages.NotFound},
+    },
+)
 async def putLikes(target_id: int, id: str = Depends(get_id)):
     return await reps.userinfo_repository.putLike(target_id, id)
 
 
-@router.put("/me/dislikes/{target_id}", response_model=list[int])
+@router.put(
+    "/me/dislikes/{target_id}",
+    responses={
+        200: {"model": list[int]},
+        404: {"model": messages.NotFound},
+    },
+)
 async def putDislikes(target_id: int, id: str = Depends(get_id)):
     return await reps.userinfo_repository.putDislike(target_id, id)
 
 
-@router.put("/me/favorites/{target_id}", response_model=list[int])
+@router.put(
+    "/me/favorites/{target_id}",
+    responses={
+        200: {"model": list[int]},
+        404: {"model": messages.NotFound},
+    },
+)
 async def putFavorites(target_id: int, id: str = Depends(get_id)):
     return await reps.userinfo_repository.putFavorite(target_id, id)
