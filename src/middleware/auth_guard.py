3b2766df4a3f3c0b9be63f88eb0c8e9ecadc2bf1
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-13 15:43:49 +0300

    fix: Fix TTL check

--- src/middleware/auth_guard.py
+++ src/middleware/auth_guard.py
@@ -14,7 +14,7 @@ async def get_id(token: str = Depends(oauth2_scheme)) -> int:
     try:
         payload = jwt.decode(token, os.getenv("JWT_TOKEN"), algorithms=["HS256"], options={"require": ["sub", "exp"]})
 
-        if payload.get('exp') >= datetime.now().timestamp():
+        if payload.get('exp') < datetime.now().timestamp():
             raise UnauthorizedException()
 
         return payload.get("sub")
