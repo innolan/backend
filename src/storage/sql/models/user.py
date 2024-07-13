ad32ff1fb2c45cb9931f0380cf17144f0e57d21d
Alexey Tkachenko <a.tkachenko@innopolis.university>
2024-07-13 13:43:17 +0300

    Reset migration history and made photo_url an array

--- src/storage/sql/models/user.py
+++ src/storage/sql/models/user.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 
-from sqlalchemy import String
+from sqlalchemy import ARRAY, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.storage.sql.__mixin__ import IdMixin
@@ -16,11 +16,14 @@ class User(Base, IdMixin):
     first_name: Mapped[str] = mapped_column(String(40))
     last_name: Mapped[Optional[str]] = mapped_column(String(40), nullable=True)
     username: Mapped[str] = mapped_column(String(60), nullable=True)
-    photo_url: Mapped[str] = mapped_column(String(200), nullable=True)
+    photo_url: Mapped[list[str]] = mapped_column(ARRAY(String(200)), nullable=True)
     auth_date_hash: Mapped[str] = mapped_column(String(200))
 
     # Relations
     profile = relationship("Profile", back_populates="user")
+    
+    user_info = relationship("UserInfo", uselist=False, back_populates="user")
+    
     _user_participant = relationship(
         "Participant", back_populates="_participant_user", cascade="all, delete-orphan"
     )
